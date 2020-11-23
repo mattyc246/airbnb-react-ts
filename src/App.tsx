e0835dc29e7a73d30a7bd4bed62650e83d6dd82b
Matthew Cross <mattyc246@gmail.com>
2020-11-23 18:18:21 +0800

    dashboard and links done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,19 @@ import './App.css';
 
 import {Switch} from "react-router-dom"
 import {PublicRoute, PrivateRoute} from "./routeHelpers"
-import { Home, Dashboard, SignUp, Login } from './pages';
+import {
+  Home,
+  Dashboard,
+  SignUp,
+  Login,
+  Profile,
+  PublicProfile,
+  ManageProfile,
+  Listings,
+  ManageListings,
+  Bookings,
+  ManageBookings
+} from './pages';
 
 function App() {
   return (
@@ -12,6 +24,13 @@ function App() {
       <PublicRoute path="/signup" restricted={true} component={SignUp} />
       <PublicRoute path="/login" restricted={true} component={Login} />
       <PrivateRoute path="/dashboard" component={Dashboard} />
+      <PublicRoute exact path="/profile" restricted={false} component={Profile} />
+      <PrivateRoute exact path="/profile/manage" component={ManageProfile} />
+      <PublicRoute  path="/profile/:id" restricted={false} component={PublicProfile} />
+      <PublicRoute exact path="/listings" restricted={false} component={Listings} />
+      <PrivateRoute path="/listings/manage" component={ManageListings} />
+      <PrivateRoute exact path="/bookings" component={Bookings} />
+      <PrivateRoute path="/bookings/manage" component={ManageBookings} />
     </Switch>
   );
 }
