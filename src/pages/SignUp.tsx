7dc0879959faa2c248b6e7824739e3e4ac2baee4
Matthew Cross <mattyc246@gmail.com>
2020-11-23 17:13:19 +0800

    sign up page complete

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import Vacation from "../assets/images/vacation.png"
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Layout, SignUpForm } from "../components"
 
-const SignUp = () => {
+const SignUp: FunctionComponent = () => {
   return (
     <Layout showFooter={true}>
       <Container fluid>
-        <Row>
-          <Col lg={7}>
-            <SignUpForm />
-          </Col>
-          <Col className="py-4" lg={5}>
+        <Row className="py-4">
+          <Col lg={{span: 5, order: 2}}>
             <h3 className="text-center">Sign up to start renting/booking today</h3>
             <img className="mx-auto d-block" src={Vacation} alt="vacation"/>
+          </Col>
+          <Col lg={{span: 7, order: 1}}>
+            <SignUpForm />
           </Col>
         </Row>
       </Container>
